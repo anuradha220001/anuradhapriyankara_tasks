3fedfc6b1e056d8782793cf76ed64dc69b8c61f9
anuradha220001 <anuradha22000@student.hamk.fi>
2023-02-01 10:54:01 +0200

    check working

--- layout/index.php
+++ layout/index.php
@@ -2,4 +2,6 @@
 <h1> some contact</h1>
 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam reprehenderit, similique est tempora fugit ducimus at explicabo sequi officia nemo ipsa voluptatum voluptas commodi, esse numquam id accusamus minus soluta!</p>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolorum officiis dolor cupiditate fugit, delectus eos aliquid odit at molestias, odio repellendus. Molestias alias aliquid, rerum fuga repudiandae cumque architecto!</p>
+
+<p> lorum </p>
 <?php include"footer.php"?>
