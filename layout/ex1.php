5fc8ae120e17f1d5946da59e6f283c127ae2da9a
anuradha220001 <anuradha22000@student.hamk.fi>
2023-02-01 11:16:56 +0200

    ex1 is done

--- layout/ex1.php
+++ layout/ex1.php
@@ -18,5 +18,39 @@ echo "Hello world! My name is \"David\"";
 <h3> 3.3 Write the PHP code in to display the current date. </h3>
 <?php echo date("d.m.Y.") ?>
 
+<h3> 3.4 Vartiable in php </h3>
+<?php
+$title1 = "PHP is interesting"; 
+echo "<h1>" .$title1 . "</h1>";
+?>
+
+<h3> 3.5 creating a table </h3>
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+echo "
+<table> 
+<tr>
+<th> S.N. </th> <th> Name </th> <th> Grade </th>
+</tr>
+<tr>
+<td> 1 </td> <td> Pekka </td> <td> $g1 </td>
+</tr>
+<tr>
+<td> 2 </td> <td> Johann </td> <td> $g2 </td>
+</tr>
+<tr>
+<td> 3 </td> <td> John </td> <td> $g3 </td>
+</tr>
+</table>
+";
+?>
+
+
+
 <?php include "footer.php" ?>
+
+
+
 
