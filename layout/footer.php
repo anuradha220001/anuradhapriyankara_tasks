5427a87481eff84d2fee08564f33479535f84569
anuradha220001 <anuradha22000@student.hamk.fi>
2023-02-07 21:37:45 +0200

    some changes to footer and no 6

--- layout/footer.php
+++ layout/footer.php
@@ -2,8 +2,12 @@
 <footer style="background-color: rgb(65, 7, 109); color: white;">
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright
-  </div>
+  <div class="footer-copyright text-center py-3">© 2020 Copyright <br>
+  <?php
+  echo "Last modifited date on  " . date("d F Y H:i");
+
+  ?>
+  
   <!-- Copyright -->
 
 </footer></div>
--- layout/variable.php
+++ layout/variable.php
@@ -1,8 +1,8 @@
 <?php include "header.php"?>
 
-<h3> Exercise 3. In class Task variables and Ooeration(variable.php) </h3>
+<h2> Exercise 3. In class Task variables and Ooeration(variable.php) </h2>
     
-<h2>Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using tag: Hello …., You are welcome to my site. </h2>
+<h3> 1. Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print using tag: Hello …., You are welcome to my site. </h3>
 
 <form action = "action.php" method = "post" >
 <div class = "row">
@@ -20,7 +20,7 @@
     </div>
     <input type ="submit" value ="submit"> 
 </form>
-4.
+<br>
 <h2> 3. Prepare a simple html table and apply bootstrap style to the table. </h2>
 <table class="table table-bordered"  >
 <tr>
@@ -36,7 +36,7 @@
 <td> 3 </td> <td> John </td> <td> 5 </td>
 </tr>
 </table>
-
+<br>
 <h2>4. Write a PHP script with two string variables. Assign any text to these variables. Join them together.  </h2>
 <?php
 $first_name = "Anuradha";
@@ -45,21 +45,20 @@ $last_name = "Priyankara";
 echo "My name is $first_name $last_name.";
 echo "<br> Lenght of my first name is " .strlen($first_name);
 ?>
-
+</n>
 <h2> 5. Write a script to add up the numbers: 298, 234, 46. </h2>
 <?php
 
 $num1 = 298;
 $num2 = 234;
 $num3 = 46;
-
 echo "Total is " . $num1 + $num2 + $num3; 
-
 ?>
+
 <h2>6. Write a PHP script to detect the browser being used to view your pages.</h2>
 <?php
-$host = $_SERVER['SCRIPT_FILENAME'];
+$host = $_SERVER['HTTP_USER_AGENT'];
 echo $host;
 ?>
 
-<?php include"footer.php"?>
+<?php include('footer.php')?>
